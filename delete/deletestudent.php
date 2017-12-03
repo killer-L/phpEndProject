26925af9588e466f45c0cdd0d0b044a87f7c0b1e
killer Li <2304806342@qq.com>
2017-12-03 22:53:49 +0800

    修改删除学生并删除成绩

--- delete/deletestudent.php
+++ delete/deletestudent.php
@@ -5,9 +5,16 @@ header ( "content-type:text/html;charset=utf-8" );
 // 取表单数据
 $id = $_REQUEST ['id'];
 // sql语句中字符串数据类型都要加引号，数字字段随便
+$sql1 = "select studentId from student where id= $id";
+$result = mysql_query($sql1);
+while ($row=mysql_fetch_array($result)){
+        $studentId=$row['studentId'];
+}
+$sql2 = "delete from score where studentId=$studentId";
+
 $sql = "delete from student where id=$id";
 //exit($sql);
-if (mysql_query ( $sql )) {
+if (mysql_query ( $sql )&&mysql_query($sql2)) {
 	echo "<script>alert('删除成功！');</script>";
 	echo "删除成功！！！<br/>";
 	echo "<a href='../student.php'>回到主页</a>";
